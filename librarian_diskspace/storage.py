0c370024ea9c23b953878226cf2f2f1d392e00cb
Branko Vukelic <branko@outernet.is>
2015-11-04 14:15:12 +0100

    Removed everything except dash plugin
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian_diskspace/storage.py
+++ librarian_diskspace/storage.py
@@ -15,8 +15,6 @@ import hwd.udev
 import hwd.storage
 from bottle import request
 
-from librarian_content.library.content import get_content_size
-
 
 def iterpath(path):
     """
@@ -74,66 +72,3 @@ def get_contentdir_storage():
     mtab_entry = find_mount_point(contentdir)
     return get_storage_by_mtab_devname(mtab_entry.dev)
 
-
-def used_space():
-    """ Return count of and total space taken by content items
-
-    :returns:   two-tuple of content count and space used by them
-    """
-
-    db = request.db.content
-    q = db.Select(['COUNT(*) AS count', 'SUM(size) AS total'],
-                  sets='content')
-    db.query(q)
-    res = db.results
-    return res[0]['count'], res[0]['total'] or 0
-
-
-def needed_space(free_space, config=None):
-    """ Returns the amount of space that needs to be freed, given free space
-
-    :param free_space:  amount of currently available free space (bytes)
-    :returns:           amount of additional space that should be freed (bytes)
-
-    """
-    config = config or request.app.config
-    return max([0, config['storage.minfree'] - free_space])
-
-
-def get_old_content(db=None):
-    """ Return content ordered from oldest to newest
-
-    :returns:   list of content ordered from oldest to newest
-    """
-    db = db or request.db.content
-    db.query("""
-             SELECT path, updated, title, views, tags, archive
-             FROM content
-             ORDER BY tags IS NULL DESC,
-                      views ASC,
-                      updated ASC,
-                      archive LIKE 'ephem%' DESC;
-             """)
-    for item in db.results:
-        yield dict((key, item[key]) for key in item.keys())
-
-
-def cleanup_list(free_space, db=None, config=None):
-    """ Return a generator of zipball metadata necessary to free enough space
-
-    The generator will stop yielding as soon as enough zipballs have been
-    yielded to satisfy the minimum free space requirement set in the
-    configuration.
-    """
-    # TODO: tests
-    items = get_old_content(db=db)
-    config = config or request.app.config
-    contentdir = config['library.contentdir']
-    space = needed_space(free_space, config=config)
-    while space > 0:
-        content_item = next(items)
-        if 'size' not in content_item:
-            content_item['size'] = get_content_size(contentdir,
-                                                    content_item['path'])
-        space -= content_item['size']
-        yield content_item
