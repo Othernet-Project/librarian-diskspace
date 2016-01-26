f058a57527e8db252895cf68c04a9b1683cd46c3
Ben Ennis <ben@outernet.is>
2016-01-26 08:15:37 -0800

    added check_diskspace task

---
+++ librarian_diskspace/tasks.py
@@ -0,0 +1,29 @@
+from . import storage
+
+
+def check_diskspace(supervisor):
+    config = supervisor.config
+    contentdir = config['library.contentdir']
+    threshold = int(config['diskspace.threshold'])
+    db = supervisor.exts.databases['notifications']
+    mtab_entry = storage.find_mount_point(contentdir)
+    sdev = storage.get_storage_by_mtab_devname(mtab_entry.dev)
+    free = int(sdev.stat.free)
+    if free < threshold:
+        supervisor.exts.notifications.send(
+            'Running low on disk space, please contact an administrator',
+            category='diskspace',
+            group='guest',
+            db=db)
+        supervisor.exts.notifications.send(
+            '%sMB of free diskspace, please remove some files' %
+            str(free/1000/1000),
+            category='diskspace',
+            group='superuser',
+            db=db)
+    else:
+        notifications = supervisor.exts.notifications.fetch_by_category('diskspace', db)
+        if not notifications:
+            return
+        for n in notifications:
+            n.delete()
