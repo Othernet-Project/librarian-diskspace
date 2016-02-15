9202b32012e0be59b0f26ce5bba870a834778f66
Andrean Franc <andrean.franc@gmail.com>
2016-02-15 13:27:05 +0100

    Revert "Register settings for enabling/disabling automatic cleanup"
    This reverts commit 8251c3a19d51ca5e3086a97d62c0bda81d2875a8.

--- librarian_diskspace/hooks.py
+++ librarian_diskspace/hooks.py
@@ -1,18 +1,9 @@
-from bottle_utils.i18n import lazy_gettext as _
-
 from .dashboard_plugin import DiskspaceDashboardPlugin
 from .tasks import check_diskspace
 
 
 def initialize(supervisor):
     supervisor.exts.dashboard.register(DiskspaceDashboardPlugin)
-    supervisor.exts.settings.add_group('storage', _("Storage settings"))
-    supervisor.exts.settings.add_field(name='auto_cleanup',
-                                       group='storage',
-                                       label=_("Automatic cleanup"),
-                                       value_type=bool,
-                                       required=False,
-                                       default=False)
 
 
 def post_start(supervisor):
