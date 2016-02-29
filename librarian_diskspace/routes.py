a83d4ce5aa95ccd003829f17b8592aa0f7dcebe1
Ben Ennis <ben@outernet.is>
2016-02-29 12:54:00 -0800

    complete overhaul of routes and associated functions

--- librarian_diskspace/routes.py
+++ librarian_diskspace/routes.py
@@ -1,33 +1,10 @@
 from .storage import get_content_storages
-from librarian_core.contrib.templates.renderer import view, template
+from librarian_core.contrib.templates.renderer import view
 
 from bottle import request
-from bottle_utils.ajax import roca_view
 
 
 _ = lambda x: x
-
-
-def start_notification(supervisor, db):
-    supervisor.exts.notifications.delete_by_category('consolidate_storage', db)
-    supervisor.exts.notifications.send(
-        # Translators, notification is displayed while files are being moved to
-        # external storage
-        _('Disk consolidation in progress.'),
-        category='consolidate_storage',
-        dismissable=False,
-        priority=supervisor.exts.notifications.CRITICAL,
-        group='guest',
-        db=db)
-    supervisor.exts.notifications.send(
-        # Translators, notification is displayed while files are being moved to
-        # external storage
-        _('Disk consolidation in progress.'),
-        category='consolidate_storage',
-        dismissable=False,
-        priority=supervisor.exts.notifications.CRITICAL,
-        group='superuser',
-        db=db)
 
 
 def success_notification(supervisor, db, paths, dest):
@@ -50,17 +27,35 @@ def success_notification(supervisor, db, paths, dest):
         db=db)
 
 
+def notification(supervisor, db, message):
+    supervisor.exts.notifications.send(
+        # Translators, notification is displayed while files are being moved to
+        # external storage
+        _(message),
+        category='consolidate_storage',
+        dismissable=True,
+        group='guest',
+        db=db)
+    supervisor.exts.notifications.send(
+        # Translators, notification is displayed while files are being moved to
+        # external storage
+        _(message),
+        category='consolidate_storage',
+        dismissable=True,
+        group='superuser',
+        db=db)
+
+
 def consolidate(supervisor, paths, dest):
     db = supervisor.exts.databases.notifications
-    response = supervisor.exts.fsal.consolidate(paths, dest)
-    import ipdb; ipdb.set_trace()
-
+    success, message = supervisor.exts.fsal.consolidate(paths, dest)
     supervisor.exts.notifications.delete_by_category('consolidate_storage', db)
-    success_notification(supervisor, db, paths, dest)
+    if success:
+        success_notification(supervisor, db, paths, dest)
+    else:
+        notification(supervisor, db, message)
 
 
-#@view('dashboard/diskspace')
-@roca_view('diskspace/dashboard', 'diskspace/_consolidate_button.tpl', template_func=template)
 def schedule_consolidate():
     """ Gets a UUID from request context, gathers a list of all drives and
     moves content from all other drives to the drive with matching UUID """
@@ -77,29 +72,19 @@ def schedule_consolidate():
     total_size = 0
     for p in paths:
         total_size += supervisor.exts.fsal.get_path_size(p)
-    if total_size < free_space:
-        #return error text
-        return {'found_storages': storages,
-            'error': "Not enough free space. {} was free, needed {}".format(
-            free_space, total_size)}
-    return {'found_storages': storages,
-            'errors': {dest_uuid: "test error from {}".format(dest_uuid)}}
+    if total_size > free_space:
+        return {'error':
+                    "Not enough free space. {} was free, needed {}".format(
+                    free_space, total_size)}
 
-    import ipdb; ipdb.set_trace()
-    consolidate(supervisor, paths, dest)
-    return {'found_storages': storages,
-            'errors': {
-                dest_uuid: "operation completed successfully from {} to {}".format(
-        paths, dest)}}
-
-    #supervisor.exts.tasks.schedule(consolidate,
-                                   #args=(supervisor, paths, dest),
-                                   #delay=0,
-                                   #periodic=False)
+    supervisor.exts.tasks.schedule(consolidate,
+                                   args=(supervisor, paths, dest),
+                                   delay=0,
+                                   periodic=False)
 
 
 
 def routes(app):
     return (
-            ('disk:consolidate', schedule_consolidate, 'POST', '/dashboard/consolidate/', {}),
+            ('disk:consolidate', schedule_consolidate, 'POST', '/consolidate/dashboard/', {}),
            )
